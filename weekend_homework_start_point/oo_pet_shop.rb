7f19e952fab800cc7e2a88179ad43d6e12f8bc5c
hatwell <caroline.hatwell@gmail.com>
2017-03-12 17:18:51 +0000

    added more functions to PetShop class and made it so it would initialize with the entire pet shop hash variable rather than several instance variables

--- weekend_homework_start_point/oo_pet_shop.rb
+++ weekend_homework_start_point/oo_pet_shop.rb
@@ -1,27 +1,77 @@
 class PetShop
-  def initialize(name, admin)
-    @name = name
-    @admin = admin
+  def initialize(pet_shop)
+    @pet_shop = pet_shop
   end
 
   def pet_shop_name
-    return @name
-  end
-
-  def who_owns_the_place
-    puts "The owner of the shop is #{@admin}"
+    return @pet_shop[:name]
   end
 
   def total_cash
-    return @admin[:total_cash]
+    return @pet_shop[:admin][:total_cash]
   end
+
+  def add_or_remove_cash(amount)
+    return @pet_shop[:admin][:total_cash] += amount
+  end
+
+  def pets_sold
+    return @pet_shop[:admin][:pets_sold]
+  end
+
+  def increase_pets_sold(shop, amount)
+    @pet_shop[:admin][:pets_sold] += amount
+  end
+
+
 end
 
-cam_admin = {
-  total_cash: 1000,
-  pets_sold: 0,
-}
+camelot_pet_shop = {
+    pets: [
+      {
+        name: "Sir Percy",
+        pet_type: :cat,
+        breed: "British Shorthair",
+        price: 500
+      },
+      {
+        name: "King Bagdemagus",
+        pet_type: :cat,
+        breed: "British Shorthair",
+        price: 500
+      },
+      {
+        name: "Sir Lancelot",
+        pet_type: :dog,
+        breed: "Pomsky",
+        price: 1000,
+      },
+      {
+        name: "Arthur",
+        pet_type: :dog,
+        breed: "Husky",
+        price: 900,
+      },
+      {
+        name: "Tristan",
+        pet_type: :dog,
+        breed: "Basset Hound",
+        price: 800,
+      },
+      {
+        name: "Merlin",
+        pet_type: :cat,
+        breed: "Egyptian Mau",
+        price: 1500,
+      }
+    ],
+    admin: {
+      total_cash: 1000,
+      pets_sold: 0,
+    },
+    name: "Camelot of Pets"
+  }
 
-camelot = PetShop.new("Camelot of Pets", cam_admin)
+camelot = PetShop.new(camelot_pet_shop)
 
-puts camelot.total_cash
+puts camelot.pet_shop_name
